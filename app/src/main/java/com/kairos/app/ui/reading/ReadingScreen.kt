9ffb7ec0eeed1e9754bf2c58c0b4a50abfd6ba96
thorin29 <97200048+thorin29@users.noreply.github.com>
2026-09-23 14:04:56 -0400

    Add files via upload

--- app/src/main/java/com/kairos/app/ui/reading/ReadingScreen.kt
+++ app/src/main/java/com/kairos/app/ui/reading/ReadingScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
